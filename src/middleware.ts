a0ed59c8c44d0b7aafaba16908ca4a39f326b120
Dylan <dylan.hochet50@gmail.com>
2024-03-31 01:44:07 +0100

    Redirect if session active or inactive

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,21 +5,25 @@ import {updateSession} from "@/services/SessionService";
 /*
  * Match all request paths except for the ones starting with:
  * - api (API routes)
- * - login
- * - register
  * - _next/static (static files)
  * - _next/image (image optimization files)
  * - favicon.ico (favicon file)
  */
 export const config = {
-    matcher: '/((?!api|login|register|_next/static|_next/image|favicon.ico).*)'
+    matcher: '/((?!api|_next/static|_next/image|favicon.ico).*)'
 }
 
 export const middleware = async (request: NextRequest) => {
     const session = await updateSession(request)
 
-    if (!session) {
-        return NextResponse.redirect(new URL('/login', request.url))
+    const isLoginPage = request.url.endsWith('/login');
+    const isRegisterPage = request.url.endsWith('/register');
+
+    if (session && (isLoginPage || isRegisterPage)) {
+        return NextResponse.redirect(new URL('/', request.url));
+    }
+    if (!session && (!isLoginPage && !isRegisterPage)) {
+        return NextResponse.redirect(new URL('/login', request.url));
     }
 
     return session
