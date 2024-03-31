84d6173f112b88b1bfc5609480b1e4545cb84d9a
Dylan <dylan.hochet50@gmail.com>
2024-03-31 01:23:13 +0100

    Comment above matcher to clarify regex

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,8 +2,17 @@ import type { NextRequest } from 'next/server'
 import {NextResponse} from "next/server";
 import {updateSession} from "@/services/SessionService";
 
+/*
+ * Match all request paths except for the ones starting with:
+ * - api (API routes)
+ * - login
+ * - register
+ * - _next/static (static files)
+ * - _next/image (image optimization files)
+ * - favicon.ico (favicon file)
+ */
 export const config = {
-    matcher: '/((?!api|_next/static|_next/image|favicon.ico|login|register).*)'
+    matcher: '/((?!api|login|register|_next/static|_next/image|favicon.ico).*)'
 }
 
 export const middleware = async (request: NextRequest) => {
